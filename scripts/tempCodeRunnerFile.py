5326ecefc1af9385a9affa6bf4dceb666b217fec
tobiashamme <tobiashammermeier@gmail.com>
2026-02-03 23:08:37 +0100

    v 0.1

---
+++ scripts/tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+LOGO_NAMEN = {
+  "SV Obertraubling II": "obertraubling.pdf",
+  "SpVgg Stadtamhof Rgbg II": "stadtamhof.png",
+  "FK Phoenix Regensburg": "phoenix.png",
+  "TV Barbing II": "barbing.pdf",
+  "(SG) Matting II/Oberndorf II": "matting_oberndorf.pdf",
+  "Kosova Regensburg II": "kosova.pdf",
+  "FSV Prüfening II": "pruefening.pdf",
+  "FC Oberhinkofen II": "oberhinkofen.pdf",
+  "TSV Oberisling Rgbg II": "oberisling.pdf",
+  "SV Burgweinting Rgbg. II": "burgweinting.pdf",
+  "VfR Regensburg II": "vfr_regensburg.pdf",
+  "NK Hrvatska Regensburg": "hrvatska.pdf",
+  "TSV Neutraubling II zg.": "neutraubling.pdf"
+}
